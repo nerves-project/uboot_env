2d0d4b663de5e4a327b2f29825d282df81755807
connor rigby <konnorrigby@gmail.com>
2018-12-19 10:44:04 -0800

    Add helpful mix tasks for viewing uboot_env

---
+++ lib/mix/tasks/uboot_env.read.ex
@@ -0,0 +1,32 @@
+defmodule Mix.Tasks.UBootEnv.Read do
+  use Mix.Task
+  import Mix.UBootEnv.Utils
+
+  @shortdoc "Read UBootEnv of a device."
+
+  @moduledoc """
+  Read UBootEnv of a device. 
+
+  Usage:
+    mix uboot_env.read PATH_TO_DEVICE DEV_OFFSET ENV_SIZE
+    mix uboot_env.read PATH_TO_CONFIG_FILE
+  """
+
+  def run([config_path]) do
+    with {:ok, kv} <- UBootEnv.read(config_path) do
+      render_kv(kv)
+    else
+      {:error, reason} -> Mix.raise(reason)
+    end
+  end
+
+  def run([dev_name, dev_offset, env_size]) do
+    with dev_offset <- parse_int(dev_offset),
+         env_size <- parse_int(env_size),
+         {:ok, kv} <- UBootEnv.load(dev_name, dev_offset, env_size) do
+      render_kv(kv)
+    else
+      {:error, reason} -> Mix.raise(reason)
+    end
+  end
+end
